7316b0cc05f00dadd9f04f2f1ae97ef2cfdd0ebc
isom <smallsol.fa@gmail.com>
2025-05-20 12:06:54 +0900

    feat : 쿠폰 중복 발급 가능 기능 추가

--- src/main/java/com/example/plusproject/domain/coupon/service/UserCouponServiceImpl.java
+++ src/main/java/com/example/plusproject/domain/coupon/service/UserCouponServiceImpl.java
@@ -41,21 +41,26 @@ public class UserCouponServiceImpl implements UserCouponService {
 			throw new IllegalArgumentException("만료된 쿠폰입니다.");
 		}
 
-		// 수량제한있으면 수량제한도 체크해야됨
-
-		long issuedCouponCount = userCouponRepository.countByCoupon_Id(couponId);
-		Long maxCoupon = coupon.getCouponQuantityIssued();
-		//null 인경우 무제한 발급
-		if (maxCoupon != null){
-			if (maxCoupon == 0 || issuedCouponCount >= maxCoupon){
-				throw new IllegalArgumentException("발급 가능한 수량이 모두 소진되었습니다.");
+		// 수량제한있으면 수량제한도 체크해야됨 / 중복 불가능만 수량 체크하게 수정
+		if (!coupon.isDuplicatePossible()){
+			long issuedCouponCount = userCouponRepository.countByCoupon_Id(couponId);
+			Long maxCoupon = coupon.getCouponQuantityIssued();
+			//null 인경우 무제한 발급
+			if (maxCoupon != null){
+				if (maxCoupon == 0 || issuedCouponCount >= maxCoupon){
+					throw new IllegalArgumentException("발급 가능한 수량이 모두 소진되었습니다.");
+				}
 			}
 		}
 
-		//받았는지 확인
-		if (userCouponRepository.existsByUser_IdAndCoupon_Id(userId,couponId)){
-			throw new IllegalArgumentException("이미 발급받은 쿠폰입니다.");
+		//중복발급 가능한지 체크 후 아니라면 -> 받았는지 확인
+		// false라면 true로 로직실행. true라면 false로 나가짐
+		if (!coupon.isDuplicatePossible()){
+			if (userCouponRepository.existsByUser_IdAndCoupon_Id(userId,couponId)){
+				throw new IllegalArgumentException("이미 발급받은 쿠폰입니다.");
+			}
 		}
+
 
 		UserCoupon userCoupon = new UserCoupon(user,coupon);
 		UserCoupon saved = userCouponRepository.save(userCoupon);
