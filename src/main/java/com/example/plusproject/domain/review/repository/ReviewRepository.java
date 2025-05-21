132bd69ef3c10586c2b6ff2988aa8fdbea9d0f15
jhyang22 <jaeho9229@gmail.com>
2025-05-21 17:52:00 +0900

    refactor : 중단 기능 삭제 및 메서드 삭제

--- src/main/java/com/example/plusproject/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/example/plusproject/domain/review/repository/ReviewRepository.java
@@ -4,11 +4,8 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import com.example.plusproject.domain.review.dto.response.ReviewSearchResponseDto;
 import com.example.plusproject.domain.review.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
@@ -18,17 +15,5 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 	}
 
 	List<Review> findAllByBook_IdAndStatus(Long bookId, boolean status);
-
-	Page<ReviewSearchResponseDto> findAllByContentContainingAndStatus(String content, boolean status);
-
-	String content(String content);
-
-	// @Query("SELECT new com.example.plusproject.domain.review.dto.response.ReviewSearchResponseDto(r.content, rl.likeReview) "
-	// 	+ "FROM ReviewLike rl "
-	// 	+ "JOIN rl.review r"
-	// 	+ "ORDER BY rl.likeReview DESC")
-	// Page<ReviewSearchResponseDto> findAllOrderByLikeDesc(Pageable pageable);
-
-	Page<Review> findAllByContentContainingAndStatus(String content, boolean status, Pageable pageable);
 
 }
